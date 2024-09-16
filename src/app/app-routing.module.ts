8343334424f838e9baa4671ad3826564704e71d4
Sebastian <sebasrojasm1@gmail.com>
2024-09-16 15:46:16 -0500

    The administration component was created to visualize the created users and their styles, and their respective path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'login' }
+  { path: 'admin', component: AdminComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
