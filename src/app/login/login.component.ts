bdf5309e090c0c5cee0ada5fda86e7f8f44e1362
Sebastian <sebasrojasm1@gmail.com>
2024-09-17 21:45:35 -0500

    The service and component was created succesfully to do the interaction with the api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+
+    if (!this.loginForm.valid) {
+      this.errorMessage = "Please provide valid credentials.";
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    this.authService.login(username, password).subscribe({
+      next: () => {
+        this.router.navigate(['/admin']);
+      },
+      error: (error : string) => {
+        // Mostrar mensaje de error en caso de fallo
+        this.errorMessage = "Invalid login credentials.";
+        console.error("Login failed", error);
+      }
+    });
+  }
 }
---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'https://users-api-phi.vercel.app/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string){
+    return this.http.post(this.apiUrl, { email, password })
+    .pipe(
+      tap((response: any) => {
+        // Almacenar el token en el localStorage
+        localStorage.setItem("token", response.token);
+      }),
+      catchError(error => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  // Método para verificar si el usuario está autenticado
+  isAuthenticated(): boolean {
+    return localStorage.getItem("token") !== null;
+  }
+
+  // Método para cerrar sesión
+  logout(): void {
+    localStorage.removeItem("token");
+  }
+}
